5bd57492b56d1d800e5b963d08732f9583d0d632
Jibreal1997 <jibrealkhan1997@gmail.com>
2022-05-28 11:39:38 -0400

    added register user form

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
 // Router files
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 
 // Connecting to MongoDb
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -87,8 +88,9 @@ app.get('/fakeuser',async(req,res)=>{
 })
 
 // Setting up routes
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 
 app.get('/', (req, res) => {
