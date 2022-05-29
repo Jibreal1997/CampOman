1667858d0001f5984773c1e92326a1763ea039c8
Jibreal1997 <jibrealkhan1997@gmail.com>
2022-05-29 07:09:22 -0400

    added auth middleware and refactored middlewares

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+
+// Middleware for checking if logged in
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         // URL to return to after logging in
@@ -6,4 +11,38 @@ module.exports.isLoggedIn = (req,res,next)=>{
        return res.redirect('/login');
     }
     next();
+}
+
+// Middleware for validating campground Schema
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+// Middleware for checking author or not
+module.exports.isAuthor = async(req,res,next)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id))
+    {
+        req.flash('error','You do not have persmission to do that');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+// Middleware for validating review schema
+module.exports.validateReview = (req,res,next)=>{
+    const {error} = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,17 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const ExpressError = require('../utils/ExpressError');
-const { reviewSchema } = require('../schemas.js');
-
-const validateReview = (req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+const {validateReview} = require('../middleware');
 
 router.post('/', validateReview, catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
